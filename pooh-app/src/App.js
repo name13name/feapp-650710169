d707470826835b258536dfc50de2c1aa6a52dbfb
อัจฉรียา ทองมาก <115211656+name13name@users.noreply.github.com>
2026-08-20 04:58:49 +0000

    1st react app

---
+++ pooh-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from './components/Card';
+import Greeting from './components/Greeting';
+
+const students = [
+  { id: 1, name: 'ฝน',   year: 3 },
+  { id: 2, name: 'เต้ย', year: 2 },
+  { id: 3, name: 'มายด์', year: 4 },
+];
+
+function App() {
+  return (
+    <div>
+      <Card>
+        <Card title="ประกาศ">
+  <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
+  <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
+</Card>
+      </Card>
+      <h1>รายชื่อนักศึกษา</h1>
+      {students.map((s) => (
+        <Greeting key={s.id} name={s.name} year={s.year} />
+      ))}
+    </div>
+    
+  );
+}
+
+export default App;
